ce035782feccaedaa750e99c10f02d929bee8a99
phuocdle96 <70562790+phuocdle96@users.noreply.github.com>
2021-06-14 13:09:09 -0700

    Predefine constant for database

---
+++ db/config.php
@@ -0,0 +1,11 @@
+<?php
+// database connect properties
+define('HOST', 'localhost');
+define('USERNAME', 'root');
+define('PASSWORD', 'password');
+define('DATABASE', 'cs160project');
+
+// define for homepage
+
+// define('CATEGORY_LIMIT', 4);
+// define('HOME_COURSES', 6);
