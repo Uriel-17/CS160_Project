16e1a598c8e3b9a808ceaa6fc5d096a486f73ecf
phuocdle96 <70562790+phuocdle96@users.noreply.github.com>
2021-06-14 13:31:19 -0700

    Add comment on category methods

--- repo/categoryRepo.php
+++ repo/categoryRepo.php
@@ -1,31 +1,37 @@
 <?php
 require_once('../db/dbhelper.php');
 
+// get all categories from database
 function getAllCategories() {
 	$sql = 'CALL GetAllCategories()';
 	return executeResult($sql);
 }
 
+// get an category by its id
 function getCategoryById($id) {
 	$sql = 'CALL GetCategoryById('.$id.')';
 	return executeSingleResult($sql);
 }
 
+// priority is for sorting
 function createCategory($categoryName, $description, $image, $priority, $createtime, $updatetime) {
 	$sql = 'CALL CreateCategory("'.$categoryName.'", "'.$description.'", "'.$image.'", '.$priority.', "'.$createtime.'", "'.$updatetime.'")';
 	execute($sql);
 }
 
+// update new category
 function updateCategory($id, $categoryName, $description, $image, $priority, $updatetime) {
 	$sql = 'CALL UpdateCategory('.$id.', "'.$categoryName.'", "'.$description.'", "'.$image.'", '.$priority.', "'.$updatetime.'")';
 	execute($sql);
 }
 
+// delete category
 function deleteCategory($id) {
 	$sql = 'CALL DeleteCategory('.$id.')';
 	execute($sql);
 }
 
+// get all categories in database but order by priority
 function getAllCategoriesInOrder() {
 	$sql = 'SELECT * FROM category ORDER BY priority';
 	return executeResult($sql);
