3e185b53398f1f31fe9c6166bba95c812c08babd
Yanqi Zhang <47930773+CarrieyqZhang@users.noreply.github.com>
2021-07-14 16:21:21 -0700

    Create login.php

---
+++ signup_login/login.php
@@ -0,0 +1,34 @@
+<?php
+    session_start();
+    $_SESSION['pageId'] = 2;
+    $title = 'User Login';   
+    include('../shared_layout/header.php');
+?>
+
+<section class="py-5">
+    <div class="container py-3">
+        <div class="row ">
+            <div class="col-lg-9 container">
+                <!-- Leave comment-->
+                <h3 class="h3 my-4 text-center">Welcome back</h3>
+                <form class="mb-5" action="registerHandler.php" method="post" enctype="multipart/form-data">
+                    <div class="row justify-content-center align-items-center">
+
+                        <div class="form-group col-lg-7 ">
+                            <input class="form-control my-3 p-2" type="text" name="username" placeholder="Username">
+                            <input class="form-control my-3 p-2" type="password" name="password" placeholder="******"
+                                required>
+                            <a href="index.php"><button class="btn3 mt-3 mb-5 " type="submit">Login</button></a>
+                            <a href="register.php" style="color:#3f8858">Register here</a>
+                        </div>
+
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php
+    include('../shared_layout/footer.php');
+?>
