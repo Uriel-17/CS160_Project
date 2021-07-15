2a38bf4a082e3a4d80e71d89ec626a3ac3cda5f4
phuocdle96 <70562790+phuocdle96@users.noreply.github.com>
2021-07-15 11:26:49 -0700

    Add query to create categories.

---
+++ Database_Query/initial_categories.sql
@@ -0,0 +1,25 @@
+USE cs160project;
+INSERT INTO category (categoryname, descrition, image, priority, createtime, updatetime) 
+VALUES ("Java", "Java is an object-oriented programming language that produces software for multiple platforms.", "java_img.png", 1, NOW(), NOW());
+
+INSERT INTO category (categoryname, descrition, image, priority, createtime, updatetime) 
+VALUES ("Python", "Python is an interpreted high-level general-purpose programming language. Python design philosophy emphasizes code readability with its notable use of significant indentation."
+, "python_img.png", 1, NOW(), NOW());
+
+INSERT INTO category (categoryname, descrition, image, priority, createtime, updatetime) 
+VALUES ("C++", "C++ is a general-purpose programming language created by Bjarne Stroustrup as an extension of the C programming language.", "cplusplus_img.png", 1, NOW(), NOW());
+
+INSERT INTO category (categoryname, descrition, image, priority, createtime, updatetime) 
+VALUES ("JavaScript", "JavaScript, often abbreviated as JS, is a programming language that conforms to the ECMAScript specification.", "javascript_img.png", 1, NOW(), NOW());
+
+INSERT INTO category (categoryname, descrition, image, priority, createtime, updatetime) 
+VALUES ("PHP", "PHP is a general-purpose scripting language especially suited to web development.", "php_img.png", 1, NOW(), NOW());
+
+INSERT INTO category (categoryname, descrition, image, priority, createtime, updatetime) 
+VALUES ("Scala", "Scala is a strong statically typed general-purpose programming language which supports both object-oriented programming and functional programming.", "scala_img.png", 1, NOW(), NOW());
+
+INSERT INTO category (categoryname, descrition, image, priority, createtime, updatetime) 
+VALUES ("Ruby", "Ruby is an interpreted, high-level, general-purpose programming language.", "ruby_img.png", 1, NOW(), NOW());
+
+INSERT INTO category (categoryname, descrition, image, priority, createtime, updatetime) 
+VALUES ("C#", "C# is a general-purpose, multi-paradigm programming language encompassing static typing, strong typing, lexically scoped, imperative, declarative, functional, generic, object-oriented, and component-oriented programming disciplines.", "csharp_img.png", 1, NOW(), NOW());
