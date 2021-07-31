5cec6e64e965d9c1316e7264b3ebfdfac7c07d54
phuocdle96 <70562790+phuocdle96@users.noreply.github.com>
2021-07-30 18:54:27 -0700

    Delete credentials.php

--- credentials.php
+++
@@ -1,9 +0,0 @@
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "password"; 
-$database = "cs160project";
-$accountTypeTable = "accounttype";
-$userTable = "user";
-$addressTable = "address"; 
-?>
